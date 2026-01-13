1c6f31b0e69e4549f27359c2ae21c9c55def4649
NandiniKunche <nandini.ists@gmail.com>
2026-01-13 21:48:23 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ require("dotenv").config();
 
 const app = express();
 
+
+
 // allow json body
 app.use(express.json());
 
@@ -43,6 +45,8 @@ const prescriptionRoutes = require("./routes/prescriptionRoutes");
 app.use("/api/prescriptions", prescriptionRoutes);
 
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // start server
 const PORT = process.env.PORT || 5000;
